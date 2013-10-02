3ab29a7373e75196bdc27e4517adef3fd080a09f
Sam Zhai <sam@tbocs.org>
2013-10-01 23:50:11 -0700

    finished first step, about to test

---
+++ src/threads/ready_list.h
@@ -0,0 +1,23 @@
+#ifndef THREADS_READY_LIST_H
+#define THREADS_READY_LIST_H
+
+#include "lib/kernel/list.h"
+#include "threads/thread.h"
+
+typedef struct {
+	struct list ready_list;
+} ready_list_t;
+
+
+static bool
+priority_less (const struct list_elem *a_, const struct list_elem *b_,
+            void *aux UNUSED) 
+{
+  const struct thread *a = list_entry (a_, struct thread, elem);
+  const struct thread *b = list_entry (b_, struct thread, elem);
+  
+  return a->priority < b->priority;
+}
+
+
+#endif
