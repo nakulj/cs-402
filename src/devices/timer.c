afce5566adc9a3e33ca347bf0a2db0fccdc235e3
Sam Zhai <sam@tbocs.org>
2013-10-11 00:26:58 -0700

    fixed the missing of thread_mlfqs guard in timer.c

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -211,16 +211,17 @@ timer_interrupt (struct intr_frame *args UNUSED)
     temp_elem = next_elem;
   }
 
-  // Update Load Average and Recent CPU every second
-  if (curr_ticks % TIMER_FREQ == 0){
-    thread_calc_load_avg();
-    // printf("\nReady Threads: %d ***", get_ready_threads_count());
-    // print_real( thread_get_load_avg() );
-    thread_calc_recent_cpu();
-  }
-
-  if (curr_ticks % 4 == 0){
-    thread_calc_priorities();
+  if (thread_mlfqs) {
+    // Update Load Average and Recent CPU every second
+    if (curr_ticks % TIMER_FREQ == 0){
+        thread_calc_load_avg();
+        // printf("\nReady Threads: %d ***", get_ready_threads_count());
+        // print_real( thread_get_load_avg() );
+        thread_calc_recent_cpu();
+    }
+    if (curr_ticks % 4 == 0){
+        thread_calc_priorities();
+    }
   }
 
   thread_tick ();
